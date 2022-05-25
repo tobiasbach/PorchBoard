86ab94a6bc73f6295c72d2374077708e1c77f9ae
Tobias Bach <tobias.bach@aroundhome.de>
2022-05-25 17:12:52 +0200

    Working on traffic query
    Currently I receive a 403 Developer inactive because the request is not properly build. Need to figure out what's wrong with it.

--- Traffic/traffic.go
+++ Traffic/traffic.go
@@ -1,4 +1,4 @@
-package traffic
+package main
 
 import (
 	"fmt"
@@ -10,19 +10,23 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func traffic() {
-	godotenv.Load(".env")
+func main() {
+	godotenv.Load("../.env")
 	versionNumber := "1"
-	destination := "52.575965519202995, 13.878845389884987"
+	destination := "52.57596,13.87884"
 	// Amiceria Strausberg
 	routePlanningLocations := fmt.Sprintf("%s:%s", os.Getenv("Home"), destination)
-	url := fmt.Sprintf("https://api.tomtom.com/routing/{%s}/calculateRoute/{%s}/{contentType}?key={%s}", versionNumber, routePlanningLocations, os.Getenv("APIKeyAccuWeather"))
+	url := fmt.Sprintf("https://api.tomtom.com/routing/%s/calculateRoute/%s/json?&key=%s", versionNumber, routePlanningLocations, os.Getenv("APIKeyAccuWeather"))
 	resp, _ := http.Get(url)
 
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
 
+	log.Println(url)
+	log.Println(os.Getenv("APIKeyAccuWeather"))
+	log.Println(routePlanningLocations)
+	log.Println(int(resp.StatusCode))
 	log.Println(string(body))
 }
 
