3e8c3b1a35d527ff453ca01682f5877dce16ab90
Tobias Bach <tobias.bach@aroundhome.de>
2023-01-21 23:44:05 +0100

    Global change: Decoding JSON to structs instead of maps

--- api_integrations/weather_forecast_accuweather/weather.go
+++ api_integrations/weather_forecast_accuweather/weather.go
@@ -16,7 +16,6 @@ func Forecast() {
 
 	// location key for 15370 Fredersdorf: '1026032'
 	// TO-DO move to env file
-
 	url := fmt.Sprintf("http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/1026032?apikey=%s&language=en&details=true&metric=true", os.Getenv("APIKeyAccuWeather"))
 	resp, _ := http.Get(url)
 
@@ -25,22 +24,19 @@ func Forecast() {
 	// convert response body to bytes:
 	body, _ := ioutil.ReadAll(resp.Body)
 
-	// convert bytes to an array to access single forecast objects
-	// 		https://stackoverflow.com/a/53538261
-	var val []map[string]interface{}
-	if err := json.Unmarshal([]byte(body), &val); err != nil {
+	// unmarshal bytes to array of structs
+	var weather []struct{ Weather }
+	if err := json.Unmarshal([]byte(body), &weather); err != nil {
 		panic(err)
 	}
 
-	// iterate over array and print value for key 'IconPhrase'
-	// 		fmt.Println(reflect.TypeOf(val)) >> []map[string]interface {}
-	// 		map[key-type]val-type
-	for i, s := range val {
+	// iterate over array of structs and return selected values (return = print as long as this is a command line tool)
+	for i, w := range weather {
 		if i == 0 {
 		} else if i == 1 {
-			fmt.Printf("Weather conditions in %s hour: %s\n", fmt.Sprint(i), s["IconPhrase"])
+			fmt.Printf("Weather conditions in %s hour: %s, %s degrees\n", fmt.Sprint(i), w.IconPhrase, fmt.Sprint(w.Temperature.Value))
 		} else {
-			fmt.Printf("Weather conditions in %s hours: %s\n", fmt.Sprint(i), s["IconPhrase"])
+			fmt.Printf("Weather conditions in %s hours: %s, %s degrees\n", fmt.Sprint(i), w.IconPhrase, fmt.Sprint(w.Temperature.Value))
 		}
 	}
 }
