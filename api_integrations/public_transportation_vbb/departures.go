aa6cb05685e43b9e9fde036cb3b48d51b87fe9fe
Tobias Bach <tobias.bach@aroundhome.de>
2023-01-27 00:00:16 +0100

    WIP Request departure board from VBB
    Need to fix 400 response in app - same request url works in Postman and browser

--- api_integrations/public_transportation_vbb/departures.go
+++ api_integrations/public_transportation_vbb/departures.go
@@ -1,22 +1,28 @@
-package main
+package vbb
 
 import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 // returns next departures from a specific stop within a time span (default 60 minutes from now)
-func departures() {
+func Departures() {
 	godotenv.Load(".env")
 
-	url := fmt.Sprintf("")
+	// calling departureBoard endpoint - requires to get the stopId first
+	url := fmt.Sprintf("%s%s%s&%s", os.Getenv("vbbBaseUrl"), os.Getenv("vbbEndpointDepartureBoard"), os.Getenv("vbbStopId"), os.Getenv("vbbAccessId"))
 	resp, _ := http.Get(url)
 
 	defer resp.Body.Close()
 
 	// convert response body to bytes:
 	body, _ := ioutil.ReadAll(resp.Body)
+
+	// TO-DO fix 400 response - url is double checked and works if pasted into a browser
+	// TO-DO XML parsing
+	fmt.Print(string(body))
 }
